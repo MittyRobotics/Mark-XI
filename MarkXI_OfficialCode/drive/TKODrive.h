dc6961b1607a82ac8486a4b527f0f3244c7eeb96
Vadim Korolik <vkorolik@gmail.com>
2014-01-10 16:10:31 -0800

    Adding missing files

---
+++ MarkXI_OfficialCode/drive/TKODrive.h
@@ -0,0 +1,21 @@
+//Last edited by Vadim Korolik
+//on 11/27/2013
+#ifndef __TKODRIVE_H
+#define __TKODRIVE_H
+
+#include "../Definitions.h"
+#include "../log/TKOLogger.h"
+#include "TKODriveBase.h"
+
+class TKODrive: public TKODriveBase
+{
+public:
+	static TKODrive* inst();
+private:
+	TKODrive();
+	~TKODrive();
+	Task *driveTask;
+	static TKODrive* m_Instance;
+	static void DriveRunner();
+};
+#endif
