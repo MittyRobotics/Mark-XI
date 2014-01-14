3dbcab2f3b300cdbd4e3e834d4b60fd6122ccb0f
Arjun Biju <arjunbiju17@mittymonarch.com>
2014-01-13 16:54:15 -0800

    Wrote a header file for the TKOIntake mechanism and wrote some code for the rollers on the arms.

---
+++ MarkXI_OfficialCode/evom/TKOIntake.h
@@ -0,0 +1,19 @@
+//Last Edited by Arjun Biju
+#ifndef __TKOINTAKE_H
+#define __TKOINTAKE_H
+
+#include "Definitions.h"
+
+//Code for intake roller
+
+class TKOIntake {
+public: 
+	TKOIntake(int port1, int port2);
+	~TKOIntake();
+	void RollerMove(bool trigger);
+private:
+	CANJaguar _roller1, _roller2;
+	
+};
+
+#endif
