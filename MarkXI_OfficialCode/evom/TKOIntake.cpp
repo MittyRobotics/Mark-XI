249d80364fc864c7c11e082886495464bb452984
Arjun Biju <arjunbiju17@mittymonarch.com>
2014-01-22 21:59:17 -0800

    Added a way to move the arms to three locations. In the middle, the lowest, and the top. Needs a little bit of work, some errors that still need to  be fixed, but you get the gist of it.

--- MarkXI_OfficialCode/evom/TKOIntake.cpp
+++ MarkXI_OfficialCode/evom/TKOIntake.cpp
@@ -1,4 +1,4 @@
-//Last Edited by Ishan Shah and Murad Awad
+//Last Edited by Arjun Biju
 #include "TKOIntake.h"
 
 /*
@@ -11,8 +11,9 @@ TKOIntake::TKOIntake():
 _roller1(6, CANJaguar::kPercentVbus),
 _roller2(7, CANJaguar::kPercentVbus),
 _arm(8, CANJaguar::kPercentVbus),
-//_switch1(2,1),
-limit1 (1), 
+limit1 (1), // Lowest limit switch
+limit2 (2), // Middle limit switch
+limit3(3),// Top limit switch
 stick1(STICK_1_PORT), // initialize joystick 1 < first drive joystick
 stick2(STICK_2_PORT), // initialize joystick 2 < second drive joystick
 stick3(STICK_3_PORT), // initialize joystick 3 < first EVOM joystick
@@ -28,7 +29,7 @@ void TKOIntake::RollerMove()
 if (stick3.GetRawButton(1)) {	//intake of ball.
 _roller1.Set(1);
 _roller2.Set(-1);
-if (limit1.Get() == true) {
+if (limit1.Get() == 0) {
 _roller1.Set(0);
 _roller2.Set(0);
 
@@ -40,21 +41,33 @@ _roller1.Set(-1);
 _roller2.Set(1);
 } else {
 _roller1.Set(0);
-_roller2.Set(0);
+_roller2.Set(0); 
 }
 }
 
-void TKOIntake::ArmMove(float y) {
-_arm.Set(y);
-}
 
-
-void TKOIntake::LimitSwitchTest()
-{ 
-if (limit1.Get() == 0)
-{ 
-printf("Sensor tripped \n");
-}
-
-
-}
+void TKOIntake::ArmMove(){ // Arm move code to move it to the lowest position
+	{
+while (stick4.GetRawButton(1));
+_arm.Set(1); // The arm starts going in a downwards fashion. This can be changed if needed.
+		if (limit1.Get()== 1);
+		_arm.Set(0);
+		printf ("The arm is at the lowest position.");
+	}
+	{
+while (stick4.GetRawButton(2)); // Arm move to middle position
+_arm.Set(1); //Arm first moves in downward fashion in case it is above the second limit switch
+		if (limit1.Get()==1); //If it is under the second limit switch, it  hits the first one, and reverts to start moving up
+		_arm.Set(-1);
+			if (limit2.Get()==1); //If it reaches the second limit switch
+			_arm.Set(0);
+			printf ("The arm has reached the middle position.");
+	}
+	{
+while (stick4.GetRawButton(3)); //Arm move to highest position
+_arm.Set(-1);
+	if (limit3.Get()==1); //The highest limit switch is triggered, and the arm is off.
+	_arm.Set(0);
+	printf ("The arm has reached the highest position");
+	}
+	}
--- MarkXI_OfficialCode/evom/TKOIntake.h
+++ MarkXI_OfficialCode/evom/TKOIntake.h
@@ -11,13 +11,13 @@ public:
 TKOIntake();
 ~TKOIntake();
 void RollerMove();
-void ArmMove(float y);
+void ArmMove();
 void LimitSwitchTest();
 //DigitalInput limit1 (int module2, int channel1);
 private:
 CANJaguar _roller1, _roller2;
 CANJaguar _arm;
-DigitalInput limit1;
+DigitalInput limit1, limit2, limit3;
 Joystick stick1, stick2, stick3, stick4;
 };
 #endif
