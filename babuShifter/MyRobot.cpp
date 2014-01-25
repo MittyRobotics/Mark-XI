707a37c810d0af12854898b2e49deeda720f97e3
Rahul Rameshbabu <rahulrameshbabu16@mittymonarch.com>
2014-01-24 16:05:52 -0800

    Manual Shift Test

--- babuShifter/MyRobot.cpp
+++ babuShifter/MyRobot.cpp
@@ -8,32 +8,33 @@ class ShifterDemo : public SimpleRobot
 {
 
 public:
-	CANJaguar drive1, drive2, drive3, drive4;
+	CANJaguar l_f, l_b, r_f, r_b;
 	Joystick stick1, stick2;
 	DoubleSolenoid s1, s2;
 	Compressor comp;
 	bool a_shift;
 	ShifterDemo():
-		drive1(DRIVE_L1_ID, CANJaguar::kPercentVbus),
-		drive2(DRIVE_L2_ID, CANJaguar::kPercentVbus),
-		drive3(DRIVE_R1_ID, CANJaguar::kPercentVbus),
-		drive4(DRIVE_R2_ID, CANJaguar::kPercentVbus),
+		l_f(DRIVE_L1_ID, CANJaguar::kPercentVbus),
+		l_b(DRIVE_L2_ID, CANJaguar::kPercentVbus),
+		r_f(DRIVE_R1_ID, CANJaguar::kPercentVbus),
+		r_b(DRIVE_R2_ID, CANJaguar::kPercentVbus),
 		stick1(STICK_1_PORT), stick2(STICK_2_PORT),
 		s1(2,1,2), s2(2,3,4),
 		comp(PRESSURE_SWITCH_PORT, COMPRESSOR_ID)
 	{
-		drive1.SetSafetyEnabled(false);
-		drive2.SetSafetyEnabled(false);
-		drive3.SetSafetyEnabled(false);
-		drive4.SetSafetyEnabled(false);
+		l_f.SetSafetyEnabled(false);
+		l_b.SetSafetyEnabled(false);
+		r_f.SetSafetyEnabled(false);
+		r_b.SetSafetyEnabled(false);
 		SetSpeedReference();
-		a_shift = true;
+		a_shift = false;
 	}
 
 	void Autonomous()
 	{
-		SetPID();
 		while (IsAutonomous()&&IsEnabled()) {
+			/*
+			 * 		SetPID();
 			if (stick2.GetTrigger()) {
 				Stop();
 				break;
@@ -43,12 +44,13 @@ public:
 			SlowestDrive();
 			Wait(25);
 			Stop();
+			*/
 		}
 	}
 	
 	void RobotInit()
 	{
-		//comp.Start();
+		comp.Start();
 	}
 
 	void OperatorControl()
@@ -69,26 +71,26 @@ public:
 	}
 	
 	void SetPID() {
-		drive1.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
-		drive2.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
-		drive3.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
-		drive4.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
+		l_f.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
+		l_b.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
+		r_f.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
+		r_b.SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
 	}
 	
 	void SetSpeedReference() {
-		drive1.SetSpeedReference(JAG_SPEEDREF);
-		drive2.SetSpeedReference(JAG_SPEEDREF);
-		drive3.SetSpeedReference(JAG_SPEEDREF);
-		drive4.SetSpeedReference(JAG_SPEEDREF);
+		l_f.SetSpeedReference(JAG_SPEEDREF);
+		l_b.SetSpeedReference(JAG_SPEEDREF);
+		r_f.SetSpeedReference(JAG_SPEEDREF);
+		r_b.SetSpeedReference(JAG_SPEEDREF);
 	}
 	
 	void AutoShift() {
-		if (min(fabs(drive1.GetSpeed()),fabs(drive2.GetSpeed())) > MAX_RPM_1
-			&& min(fabs(drive3.GetSpeed()),fabs(drive4.GetSpeed())) > MAX_RPM_1) {
+		if (fabs(l_f.GetSpeed()) > MAX_RPM_1
+			&& fabs(r_f.GetSpeed()) > MAX_RPM_1) {
 			shiftToHighGear();
 		}
-		else if(max(fabs(drive1.GetSpeed()),fabs(drive2.GetSpeed())) < MIN_RPM_1
-		   || max(fabs(drive3.GetSpeed()),fabs(drive4.GetSpeed())) < MIN_RPM_1) {
+		else if(fabs(l_f.GetSpeed()) < MIN_RPM_1
+		   || fabs(r_f.GetSpeed()) < MIN_RPM_1) {
 			shiftToLowGear();
 		}
 	}
@@ -105,40 +107,46 @@ public:
 	
 	void ManualShift() {
 		if (stick2.GetRawButton(3)) {
-			if (s1.Get() == s1.kForward)
-				s1.Set(s1.kReverse);
-			else
+			if (s1.Get() == s1.kReverse)
 				s1.Set(s1.kForward);
-			if (s2.Get() == s2.kForward)
-				s2.Set(s2.kReverse);
 			else
+				s1.Set(s1.kReverse);
+			if (s2.Get() == s2.kReverse)
 				s2.Set(s2.kForward);
+			else
+				s2.Set(s2.kReverse);
 		}
+		if (stick1.GetTrigger()) {
+			s1.Set(s1.kOff);
+			s2.Set(s2.kOff);
+		}
+		Wait(1); ///TODO TEMP fix, need so holding down button wont toggle back and forth
+		
 	}
 	
 	void FullDrive() {
-		drive1.Set(-1.0);
-		drive2.Set(-1.0);
-		drive3.Set(1.0);
-		drive4.Set(1.0);
+		l_f.Set(-1.0);
+		l_b.Set(-1.0);
+		r_f.Set(1.0);
+		r_b.Set(1.0);
 		AutoShift();
 	}
 	
 	void SlowestDrive() {
-		drive1.Set(-0.35);
-		drive2.Set(-0.35);
-		drive3.Set(0.35);
-		drive4.Set(0.35);
+		l_f.Set(-0.35);
+		l_b.Set(-0.35);
+		r_f.Set(0.35);
+		r_b.Set(0.35);
 		AutoShift();
 	}
 	
 	void Stop() {
 		shiftToLowGear();
 		Wait(0.5);
-		drive1.Set(0);
-		drive2.Set(0);
-		drive3.Set(0);
-		drive4.Set(0);
+		l_f.Set(0);
+		l_b.Set(0);
+		r_f.Set(0);
+		r_b.Set(0);
 	}
 	
 	void ShiftControl() {
@@ -153,46 +161,27 @@ public:
 	}
 	
 	void TankDrive() {
-		if (stick1.GetTrigger())
-			{
-				drive1.Set(-stick1.GetY() * 0.4);
-				drive2.Set(-stick1.GetY() * 0.4);
-				drive3.Set(stick2.GetY() * 0.4);
-				drive4.Set(stick2.GetY() * 0.4);
-				ShiftControl();
-			}
-			else if (stick1.GetRawButton(2))
-			{
-				drive1.Set(-stick1.GetY() * 0.2);
-				drive2.Set(-stick1.GetY() * 0.2);
-				drive3.Set(stick2.GetY() * 0.2);
-				drive4.Set(stick2.GetY() * 0.2);
-				ShiftControl();
-			}
-			else if (stick1.GetRawButton(4))
-			{
-				drive1.Set(-stick1.GetY());
-				drive2.Set(-stick1.GetY());
-				drive3.Set(stick2.GetY());
-				drive4.Set(stick2.GetY());
-				ShiftControl();
-			}
-			else if (stick2.GetRawButton(2))
-			{
-				drive1.Set(-stick1.GetY());
-				drive2.Set(-stick1.GetY());
-				drive3.Set(stick1.GetY());
-				drive4.Set(stick1.GetY());
-				ShiftControl();
-			}
-			else
-			{
-				drive1.Set(-stick1.GetY() * 0.8);
-				drive2.Set(-stick1.GetY() * 0.8);
-				drive3.Set(stick2.GetY() * 0.8);
-				drive4.Set(stick2.GetY() * 0.8);
-				ShiftControl();
-			}
+		if(stick1.GetY()>0.0||stick2.GetY()>0.0) {
+					l_f.Set(-stick1.GetY() * 0.9);
+					r_f.Set(stick2.GetY() * 0.9);
+					l_b.Set(-stick1.GetY() * 0.9);
+					r_b.Set(stick2.GetY() * 0.9);
+					ShiftControl();
+				}
+				else if (stick1.GetY()<0.0||stick2.GetY()<0.0) {
+					l_b.Set(-stick1.GetY() * 0.9);
+					r_b.Set(stick2.GetY() * 0.9);
+					l_f.Set(-stick1.GetY() * 0.9);
+					r_f.Set(stick2.GetY() * 0.9);
+					ShiftControl();
+				}
+				else {
+					l_f.Set(0.0);
+					l_b.Set(0.0);
+					r_f.Set(0.0);
+					r_b.Set(0.0);
+					ShiftControl();
+				}
 	}
 };
 
