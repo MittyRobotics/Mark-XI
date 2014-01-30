eb3a65baf704682711ea5290959b8d7f79826c71
Murad Awad <muradawar17@mittymonarch.com>
2014-01-29 16:45:14 -0800

    Almost done with arm code.

--- MarkXI_OfficialCode/evom/TKOIntake.cpp
+++ MarkXI_OfficialCode/evom/TKOIntake.cpp
@@ -14,13 +14,13 @@ TKOIntake::TKOIntake() :
 			_roller1(6, CANJaguar::kPercentVbus),
 			_roller2(7, CANJaguar::kPercentVbus),
 			_arm1(8, CANJaguar::kPercentVbus),
-			_arm2(9, CANJaguar::kPercentVbus), limitLow(1), // Lowest limit switch
-			limitMiddle(2), // Middle limit switch
-			limitHigh(3),// Top limit switch
-			limitIn(4), stick1(STICK_1_PORT), // initialize joystick 1 < first drive joystick
+		    LimitSwitchArm(1), // Optical limit switch
+		    limitIn(2),// Limit switch in arms
+		    stick1(STICK_1_PORT), // initialize joystick 1 < first drive joystick
 			stick2(STICK_2_PORT), // initialize joystick 2 < second drive joystick
 			stick3(STICK_3_PORT), // initialize joystick 3 < first EVOM joystick
-			stick4(STICK_4_PORT) // initialize joystick 4 < second EVOM joystick-m,
+			stick4(STICK_4_PORT), // initialize joystick 4 < second EVOM joystick-m,
+			ArmEncoder(1,8,1,9,false)
 {
 	printf("Initializing intake\n");
 	intakeTask = new Task("TKOIntake", (FUNCPTR) IntakeRunner);
@@ -61,41 +61,45 @@ void TKOIntake::RollerMoveOut() {
  _roller2.Set(0);*/
 
 void TKOIntake::ArmMoveLow() { // Arm move code to move it to the lowest position
+	ArmEncoder.Start();
 
 	_arm1.Set(1); // The arm starts going in a downwards fashion. This can be changed if needed.
-	_arm2.Set(1);
-	if (limitLow.Get() == 0) {
+	if (LimitSwitchArm.Get() == 1) {
 		_arm1.Set(0);
-		_arm2.Set(0);
+		
+		ArmEncoder.Get();
 		printf("The arm is at the lowest position.");
 
 	}
 }
 
 void TKOIntake::ArmMoveMiddle() {
+	
 	_arm1.Set(1); //Arm first moves in downward fashion in case it is above the second limit switch
-	_arm2.Set(1);
-	if (limitLow.Get() == 0) {//If it is under the second limit switch, it  hits the first one, and reverts to start moving up
-		_arm1.Set(-1);
-		_arm2.Set(1);
-	}
-	if (limitMiddle.Get() == 0) { //If it reaches the second limit switch
+	if (LimitSwitchArm.Get() == 1) {//If it is under the second limit switch, it  hits the first one, and reverts to start moving up
 		_arm1.Set(0);
-		_arm2.Set(0);
-		printf("The arm has reached the middle position.");
+		ArmEncoder.Start();
+		_arm1.Set(-1);
+	if (LimitSwitchArm.Get()==1)
+	_arm1.Set(0);
+	ArmEncoder.Get();
+
 	}
+
 }
 
 void TKOIntake::ArmMoveHigh() {
 	_arm1.Set(-1);
-	_arm2.Set(-1);
-	if (limitHigh.Get() == 1) {//The highest limit switch is triggered, and the arm is off.
+	if (LimitSwitchArm.Get() == 1) {//The highest limit switch is triggered, and the arm is off.
 		_arm1.Set(0);
-		_arm2.Set(0);
+		
 		printf("The arm has reached the highest position");
 	}
 }
-
+void TKOIntake::ArmMoveManual()
+{
+	_arm1.Set(stick3.GetY());
+}
 void TKOIntake::IntakeRunner() {
 	while (true) {
 
--- MarkXI_OfficialCode/evom/TKOIntake.h
+++ MarkXI_OfficialCode/evom/TKOIntake.h
@@ -17,15 +17,16 @@ public:
 	void ArmMoveHigh();
 	void LimitSwitchTest();
 	void RollerMoveOut();
-
+	void ArmMoveManual();
 private:
 	DISALLOW_COPY_AND_ASSIGN(TKOIntake);
 	static TKOIntake* m_Instance;
 	static void IntakeRunner();
 	Task *intakeTask;
 
-	CANJaguar _roller1, _roller2, _arm1, _arm2;
-	DigitalInput limitLow, limitMiddle, limitHigh, limitIn;
+	CANJaguar _roller1, _roller2, _arm1;
+	DigitalInput LimitSwitchArm, limitIn;
 	Joystick stick1, stick2, stick3, stick4;
+	Encoder ArmEncoder;
 };
 #endif
