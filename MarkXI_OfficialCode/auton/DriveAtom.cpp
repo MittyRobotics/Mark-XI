011b2a1ce8bc9230746a8f81315f1bd59afcc605
Vadim Korolik <vkorolik@gmail.com>
2014-02-17 20:39:59 -0800

    Added ishan auton

--- MarkXI_OfficialCode/auton/DriveAtom.cpp
+++ MarkXI_OfficialCode/auton/DriveAtom.cpp
@@ -1,37 +1,65 @@
-//Last Edited by Ishan Shah and Alex Parks
+//Last Edited by Ishan Shah
 #include "DriveAtom.h"
 #include <cstring>
 // new branch
-
-DriveAtom::DriveAtom(float feet, CANJaguar* drive1, CANJaguar* drive2,
-		CANJaguar* drive3, CANJaguar* drive4) {
-	const float REVS_PER_FOOT = 0.7652439024;
-	_distance = feet * REVS_PER_FOOT;
+DriverStation* ds;
+DriveAtom::DriveAtom(float feet, CANJaguar* drive1, CANJaguar* drive2, CANJaguar* drive3, CANJaguar* drive4) {
+	_distance = feet; //* REVS_PER_FOOT;
 	_drive1 = drive1;
 	_drive2 = drive2;
 	_drive3 = drive3;
 	_drive4 = drive4;
+//	kP = 0.7;
+//	kI = 0.00;//23;
+//	kD = 0.0;
+	ds = DriverStation::GetInstance();
 }
 
 DriveAtom::~DriveAtom() {
 }
 
 void DriveAtom::run() {
-	ds = DriverStation::GetInstance();
-	//don't forget to divide number of rotations by REVS_PER_FOOT in order to get feet traveled
+	
+	
+	//don't forget to divide nu--mber of rotations by REVS_PER_FOOT in order to get feet traveled
+
+	_drive1->SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
+	_drive3->SetPID(DRIVE_kP, DRIVE_kI, DRIVE_kD);
 	_drive1->EnableControl(0);
 	_drive3->EnableControl(0);
+	while (!((_drive1->GetPosition() <= _distance + 0.5) and (_drive1->GetPosition() >= _distance - 0.5))) {//TODO FFS FIX THIS SHIT
 
-	while ((_drive1->GetPosition() >= _distance - .2) or (_drive1->GetPosition() <= _distance + .2)
-			and (_drive3->GetPosition() >= _distance - .2) or (_drive3->GetPosition() <= _distance + .2)
-			and ds->IsEnabled()) {
-		//need a +/- difference for distance
+//		while(kP != ds->GetAnalogIn(1)*0.2) {
+			//kP = ds->GetAnalogIn(1)*0.2;
+//			_drive1->SetPID(kP, kI, kD);
+//			_drive3->SetPID(kP, kI, kD);
+//		}
+//		while(kI != ds->GetAnalogIn(2)*0.0008) {
+//			kI = ds->GetAnalogIn(2)*0.0008;
+//			_drive1->SetPID(kP, kI, kD);
+//			_drive3->SetPID(kP, kI, kD);
+//		}
+		
 		printf("drive 1: %f, drive 3: %f, distance: %f\n",
 				_drive1->GetPosition(), _drive3->GetPosition(), _distance);
+		
+		printf("breaking? %d\n", !((_drive1->GetPosition() <= _distance + 0.5) and (_drive1->GetPosition() >= _distance - 0.5)));
+				
 		_drive1->Set(_distance);
-		_drive2->Set(-_drive1->GetOutputVoltage() / _drive1->GetBusVoltage()); //sets second jag to slave			
-		_drive3->Set(-(_distance));
-		_drive4->Set(-_drive3->GetOutputVoltage() / _drive3->GetBusVoltage()); //sets fourth jag to slave
+		_drive2->Set(_drive1->GetOutputVoltage() / _drive1->GetBusVoltage()); //sets second jag to slave			
+		_drive3->Set(-_distance);
+		_drive4->Set(_drive3->GetOutputVoltage() / _drive3->GetBusVoltage()); //sets fourth jag to slave
+		
 	}
-
+	printf("drive 1: %f, drive 3: %f, distance: %f\n",
+					_drive1->GetPosition(), _drive3->GetPosition(), _distance);
+	printf("broke %d\n", !((_drive1->GetPosition() <= _distance + 0.5) and (_drive1->GetPosition() >= _distance - 0.5)));
+	
+	_drive1->DisableControl();
+	_drive2->Set(0);
+	_drive3->DisableControl();
+	_drive4->Set(0);
+	Wait(1.0); 
+	
+	
 }
--- MarkXI_OfficialCode/auton/DriveAtom.h
+++ MarkXI_OfficialCode/auton/DriveAtom.h
@@ -10,8 +10,18 @@ public:
 	// For feet, positive is moving forward in relation to the robot
 	DriveAtom(float feet, CANJaguar* drive1, CANJaguar* drive2, CANJaguar* drive3, CANJaguar* drive4);
 	~DriveAtom();
+	float kP;
+	float kI;
+	float kD;
+	DriverStation* ds;
+	void SetPID(float _kP, float _kI, float _kD);
+	void SetP(float _kP);
+	void SetI(float _kI);
+	void SetD(float _kD);
+	float GetP(); 
+	float GetI(); 
+	float GetD(); 
 	void run();
-	DriverStation *ds;
 private:
 	CANJaguar* _drive1;
 	CANJaguar* _drive2;
