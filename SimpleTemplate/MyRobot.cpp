8d26205713fb061bcb145d954105391a6d12dc96
Vadim Korolik <vkorolik@gmail.com>
2014-03-13 12:07:55 -0700

    changes

--- SimpleTemplate/MyRobot.cpp
+++ SimpleTemplate/MyRobot.cpp
@@ -2,17 +2,16 @@
 
 class RobotDemo : public SimpleRobot
 {
-	CANJaguar _arm;
+	Relay relay2, relay3, relay4, relay5;
+	Compressor comp;
 public:
 	RobotDemo():
-		_arm(7, CANJaguar::kPercentVbus)
+		relay2(2, Relay::kBothDirections),
+		relay3(3, Relay::kBothDirections),
+		relay4(4, Relay::kBothDirections),
+		relay5(5, Relay::kBothDirections),
+		comp(3,1)
 	{}
-	
-	void RobotInit()
-	{
-		_arm.SetPositionReference(_arm.kPosRef_QuadEncoder);
-		_arm.ConfigEncoderCodesPerRev(250);
-	}
 
 	void Autonomous()
 	{
@@ -24,13 +23,57 @@ public:
 		printf("STARTING OPERATOR\n");
 		while (IsOperatorControl() && IsEnabled())
 		{
-			printf("arm: %f\n", _arm.GetPosition());
+			for (int i = 0; i < 1; i++)
+			{
+				if (true)
+				{
+					printf("Seding data\n");
+				
+					relay2.Set(relay2.kForward);
+					relay3.Set(relay3.kForward);
+					relay4.Set(relay4.kOff);
+					relay5.Set(relay5.kOff);//record data on arduino
+					Wait(0.1);
+					relay5.Set(relay5.kForward);
+					Wait(2.);
+				}
+				relay5.Set(relay5.kForward);
+				relay2.Set(relay2.kForward);
+				relay3.Set(relay3.kForward);
+				relay4.Set(relay4.kForward);
+				Wait(0.1);
+			}
+			Wait(10.);
+			for (int i = 0; i < 1; i++)
+			{
+				if (true)
+				{
+					printf("Seding data\n");
+					relay2.Set(relay2.kForward);
+					relay3.Set(relay3.kOff);
+					relay4.Set(relay4.kOff);
+					relay5.Set(relay5.kOff);//record data on arduino
+					Wait(0.1);
+					relay5.Set(relay5.kForward);
+					Wait(2.);
+				}
+				relay5.Set(relay5.kForward);
+				relay2.Set(relay2.kForward);
+				relay3.Set(relay3.kForward);
+				relay4.Set(relay4.kForward);
+				Wait(0.1);
+			}
+			Wait(10.);
 		}
 	}
 
 	void Test() 
 	{
-
+		comp.Start();
+		while (IsEnabled())
+		{
+		}
+		comp.Stop();
 	}
 };
 
