132a4378fd401967a9cad6e9df75c4435f16e6b5
Ben Kim <BumjinKim16@mittymonarch.com>
2014-06-14 10:15:16 -0700

    crio crash test did not crash

--- cRioCrashTest/MyRobot.cpp
+++ cRioCrashTest/MyRobot.cpp
@@ -6,53 +6,49 @@
  * The SimpleRobot class is the base of a robot application that will automatically call your
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
- */ 
-class RobotDemo : public SimpleRobot
-{
-swag test;
+ */
+
+class RobotDemo: public SimpleRobot {
+	swag test;
+
 public:
-	RobotDemo():
+	RobotDemo() :
 		test()
+	{}
+
+	void Disabled()
 	{
+		printf("Robot successfully died!\n");
+		while (IsDisabled())
+		{
+			Wait(.1);
+		}
 	}
 
-	/**
-	 * Drive left & right motors for 2 seconds then stop
-	 */
-	void Autonomous()
-	{
-	}
-
-	/**
-	 * Runs the motors with arcade steering. 
-	 */
+	void Autonomous() {}
+	
 	void OperatorControl()
 	{
 		printf("Starting the swag\n");
 		Wait(0.5);
-		
-	    printf("Hello world! Inside MyRobot.cpp \n");
-	    ///swag::getPointer()->swagOut(); <<<<<<<<<<<<<<<< uncomment this to explode, null pointer test
-	    ///printf("%f", 1.0/0.0); <<<<<<<<<<<<<<<<<<<<<<<< uncomment this to explode, divide by zero
-	    //////BEFORE UNCOMMENTING THE TESTS
-	    /*
-	     * Make sure the code works (and prints to netconsole) as it is
-	     */
 
-		while (IsOperatorControl())
-		{
-			Wait(0.005);				// wait for a motor update time
-		}
+		printf("Hello world! Inside MyRobot.cpp \n");
 		
+		swag::getPointer()->swagOut(); //prints out "0.000000 swagging out", code does not crash
+		
+		printf("Dividing by zero...\t%f", 1.0/0.0); //prints out "Inf", code does not crash
+		
+		while (IsOperatorControl() and IsEnabled())
+		{
+			Wait(0.005); // wait for a motor update time
+		}
+
 	}
-	
-	/**
-	 * Runs during test mode
-	 */
+
 	void Test() {
 
 	}
 };
 
-START_ROBOT_CLASS(RobotDemo);
-
+START_ROBOT_CLASS(RobotDemo)
+;
